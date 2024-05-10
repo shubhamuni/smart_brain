d9a4a834fe17b037e9853ce700a04189769c876a
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-10 21:02:53 +0530

    updating style

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@
 
 const Navigation = () => {
     return (
-        <nav style={{display:"flex",justifyContent:"flex-end", backgroundColor:"gray"}}>
+        <nav style={{display:"flex",justifyContent:"flex-end"}}>
             <p className="f3 link dim black underline pr3 pointer">Sign Out</p>
         </nav>
     )

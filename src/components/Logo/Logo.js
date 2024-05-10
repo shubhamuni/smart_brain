9c790e9c157b597230ea75b501e137be83f0a191
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-10 21:37:58 +0530

    react-tilt

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,11 @@
-
+import { Tilt } from 'react-tilt'
+import "./Logo.css"
 const Logo = () => {
     return (
     <div className="ma4 mt0">
-
+        <Tilt className="Tilt br2 shadow-2" options={{ max:65 }} style={{ height: 150, width: 150 }}>
+            <div className='Tilt-inner'>👽</div>
+        </Tilt>
     </div>       
     )
 }

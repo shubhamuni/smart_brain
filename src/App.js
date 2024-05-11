c0e99646f19effe9716f7c31cbd3df0c2a24dec1
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-11 06:18:16 +0530

    Rank component placing

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
     <Navigation/>
     <Logo/>
-    <ImageLinkForm/>
     <Rank/>
+    <ImageLinkForm/>
     </div>
   );
 }
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Rank = () => {
     return(
-        <div></div>
+        <div>
+            <div className="white f3">
+                {'Shubham Your current rank is #1'}
+            </div>
+        </div>
 
     )
 }

45fe4979172e79ebf44a45fad620cb8b3e64f710
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-14 12:20:50 +0530

    check parameters

--- src/App.js
+++ src/App.js
@@ -7,9 +7,81 @@ import Rank from './components/Rank/Rank';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 import { loadSlim } from "@tsparticles/slim";
 
+
 function App() {
   const [ init, setInit ] = useState(false);
   const [input, setInput] = useState('');
+  
+  const PAT = '8768d5deea344d72aa5cb2d3aa849640';
+
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+
+const USER_ID = 'clarifai';
+const APP_ID = 'main';
+
+// Change these to whatever model and image URL you want to use
+
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+const IMAGE_URL = input;
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+///////////////////////////////////////////////////////////////////////////////////
+
+const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                    // "base64": IMAGE_BYTES_STRING
+                }
+            }
+        }
+    ]
+
+});
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+
+// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+// this will default to the latest version_id
+
+fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        const regions = result.outputs[0].data.regions;
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+            });
+        });
+    })
+    .catch(error => console.log('error', error));
 
   const onInputChange = (event) => {
     setInput(event.target.value)

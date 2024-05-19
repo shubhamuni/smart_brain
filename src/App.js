d42a4987c99f162209859232ff9d31cbf76b4ece
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-19 08:35:50 +0530

    element changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Rank from './components/Rank/Rank';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 import { loadSlim } from "@tsparticles/slim";
 import FaceRecognition from './components/Face-Recognition/FaceRecognition';
-import Signin from './components/SignIn/Signin';
+import Signin from "./components/SignIn/Signin.1";
 
 function App() {
   const [ init, setInit ] = useState(false);

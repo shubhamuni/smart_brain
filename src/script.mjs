2ce359fea5aa48c702b0e719f177afd311a8781e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-22 19:28:28 +0530

    Top level await

--- src/script.mjs
+++ src/script.mjs
@@ -3,6 +3,6 @@ import { largeNumber } from "./script1.mjs";
 
 
 const a = largeNumber;
-const b = 341;
-
-console.log(a+b);
+const response = await fetch("https://jsonplaceholder.typicode.com/users");
+const data = await response.json();
+console.log(data);

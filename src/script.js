fe9e5b88e22faa2399e56b267046f6e729df7075
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-22 21:11:00 +0530

    node create server with http

--- src/script.js
+++ src/script.js
@@ -1,9 +1,14 @@
-// let a;
-// if(1 === 1) {
-//     const { largeNumber } = await import("./script1.js");
-//     a = largeNumber
-// }
+import { createServer } from "http";
+const server = createServer(()=>{
+    console.log("I hear you, thanks for request")
+})
+server.listen(3001);
+let a;
+if(true) {
+    const { largeNumber } = await import("./script1.js");
+    a = largeNumber
+}
 
-// // const response = await fetch("https://jsonplaceholder.typicode.com/users");
-// // const data = await response.json();
-// console.log(a);
+// const response = await fetch("https://jsonplaceholder.typicode.com/users");
+// const data = await response.json();
+console.log(a);

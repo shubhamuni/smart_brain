819ca13870f4650d76cff424f57598ae6e5e70c9
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-22 20:48:34 +0530

    resolving errors

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-let a;
-if(1 === 1) {
-    const { largeNumber } = await import("./script1.js");
-    a = largeNumber
-}
+// let a;
+// if(1 === 1) {
+//     const { largeNumber } = await import("./script1.js");
+//     a = largeNumber
+// }
 
-// const response = await fetch("https://jsonplaceholder.typicode.com/users");
-// const data = await response.json();
-console.log(a);
+// // const response = await fetch("https://jsonplaceholder.typicode.com/users");
+// // const data = await response.json();
+// console.log(a);

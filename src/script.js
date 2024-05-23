815865ac57614212fc0b8d72dd0d9263ab56854f
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-23 07:56:07 +0530

    express check

--- src/script.js
+++ src/script.js
@@ -1,14 +1,19 @@
-import { createServer } from "http";
-const server = createServer(()=>{
-    console.log("I hear you, thanks for request")
-})
-server.listen(3001);
-let a;
-if(true) {
-    const { largeNumber } = await import("./script1.js");
-    a = largeNumber
-}
+const express = require("express")
 
-// const response = await fetch("https://jsonplaceholder.typicode.com/users");
-// const data = await response.json();
-console.log(a);
+
+
+
+// import { createServer } from "http";
+// const server = createServer(()=>{
+//     console.log("I hear you, thanks for request")
+// })
+// server.listen(3001);
+// let a;
+// if(true) {
+//     const { largeNumber } = await import("./script1.js");
+//     a = largeNumber
+// }
+
+// // const response = await fetch("https://jsonplaceholder.typicode.com/users");
+// // const data = await response.json();
+// console.log(a);

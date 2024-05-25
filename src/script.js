8e49da47294b7ecbf72024b257ee641199b1bfe6
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-25 08:36:32 +0530

    console.log(error)

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 fs.readFile('/hello.txt',(err, data) => {
     if(err){
-        console.log(err)
+        console.log('Error',err)
     }
     console.log('Async', data.toString('utf-8'))
 })

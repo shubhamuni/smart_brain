bba895c7964f9c23ed93904b560979b187f6f80b
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-27 20:56:20 +0530

    reset

--- src/script.js
+++ src/script.js
@@ -1,30 +1,30 @@
-// const express = require("express")
-// const fs = require("fs")
+const express = require("express")
+const fs = require("fs")
 
 
-// fs.readFile('./hello.txt',(err, data) => {
-//     if(err){
-//         console.log('Error',err)
-//     }
-//     console.log('Async', data.toString('utf-8'))
-// })
-// const file = fs.readFileSync('./hello.txt');
-// console.log('Sync',file.toString())
-// //Below line adds " this is test" sentence to end of hello.txt content
-// fs.appendFile('./hello.txt', " this is test", err => console.log(err))
+fs.readFile('./hello.txt',(err, data) => {
+    if(err){
+        console.log('Error',err)
+    }
+    console.log('Async', data.toString('utf-8'))
+})
+const file = fs.readFileSync('./hello.txt');
+console.log('Sync',file.toString())
+//Below line adds " this is test" sentence to end of hello.txt content
+fs.appendFile('./hello.txt', " this is test", err => console.log(err))
 
-// const app = express();
+const app = express();
 
-// app.use(express.static(__dirname + '../../public'))
+app.use(express.static(__dirname + '../../public'))
 
-// app.listen(3001);
+app.listen(3001);
 
-let a;
-if(true) {
-    const { largeNumber } = await import("./script1.js");
-    a = largeNumber
-}
-console.log(a)
+// let a;
+// if(true) {
+//     const { largeNumber } = await import("./script1.js");
+//     a = largeNumber
+// }
+// console.log(a)
 // // const response = await fetch("https://jsonplaceholder.typicode.com/users");
 // // const data = await response.json();
 // console.log(a);

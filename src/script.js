b80c9c13e61d0bdbb0c514f8f3ef8a33cfc54225
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-27 20:55:59 +0530

    when write ECMA Script 2022 then we have to mention type = module in package.json

--- src/script.js
+++ src/script.js
@@ -1,29 +1,33 @@
-const express = require("express")
-const fs = require("fs")
+// const express = require("express")
+// const fs = require("fs")
+
+
+// fs.readFile('./hello.txt',(err, data) => {
+//     if(err){
+//         console.log('Error',err)
+//     }
+//     console.log('Async', data.toString('utf-8'))
+// })
+// const file = fs.readFileSync('./hello.txt');
+// console.log('Sync',file.toString())
+// //Below line adds " this is test" sentence to end of hello.txt content
+// fs.appendFile('./hello.txt', " this is test", err => console.log(err))
+
+// const app = express();
+
+// app.use(express.static(__dirname + '../../public'))
+
+// app.listen(3001);
+
 let a;
-if (true){
-    const { largeNumber } = await import('./script1.js');
-    a = largeNumber;
+if(true) {
+    const { largeNumber } = await import("./script1.js");
+    a = largeNumber
 }
-
-console.log(a+1);
-
-fs.readFile('./hello.txt',(err, data) => {
-    if(err){
-        console.log('Error',err)
-    }
-    console.log('Async', data.toString('utf-8'))
-})
-const file = fs.readFileSync('./hello.txt');
-console.log('Sync',file.toString())
-//Below line adds " this is test" sentence to end of hello.txt content
-fs.appendFile('./hello.txt', " this is test", err => console.log(err))
-
-const app = express();
-
-app.use(express.static(__dirname + '../../public'))
-
-app.listen(3001);
+console.log(a)
+// // const response = await fetch("https://jsonplaceholder.typicode.com/users");
+// // const data = await response.json();
+// console.log(a);
 // console.log(__dirname)
 // import { createServer } from "http";
 // const server = createServer(()=>{

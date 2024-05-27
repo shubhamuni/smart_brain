eafc37345bcaff24506e32d2dc86e1d4445da38a
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-27 18:59:29 +0530

    top level await

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const fs = require("fs")
 
+const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+const data = await response.json();
+
+console.log(data);
+
 fs.readFile('./hello.txt',(err, data) => {
     if(err){
         console.log('Error',err)

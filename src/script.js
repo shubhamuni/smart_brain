8e91c0e45af4f6a6b4d64920d3f4ea923785954b
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-29 12:06:34 +0530

    console.time&console.timeEnd

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const fs = require("fs")
 
+console.time("startcheck")
 
 fs.readFile('./hello.txt',(err, data) => {
     if(err){
@@ -31,6 +32,7 @@ app.use((req, res, next)=>{
     console.log("Hello")
 }) 
 app.listen(3001);
+console.timeEnd("startcheck")
 // let a;
 // if(true) {
 //     const { largeNumber } = await import("./script1.js");

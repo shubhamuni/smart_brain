fade49cba33a30cf753f020cf5fae316b3e4d283
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-29 16:55:57 +0530

    changes crashing

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ fs.unlink("./bye.txt",err =>
   }
   console.log("Inception")
 })
-
+ 
 const app = express();
 
 app.use(express.static(__dirname + '../../public'))
@@ -33,12 +33,12 @@ app.use((req, res, next)=>{
 }) 
 app.listen(3001);
 console.timeEnd("startcheck")
-// let a;
-// if(true) {
-//     const { largeNumber } = await import("./script1.js");
-//     a = largeNumber
-// }
-// console.log(a)
+let a;
+if(true) {
+    const { largeNumber } = await import("./script1.js");
+    a = largeNumber
+}
+console.log(a)
 // // const response = await fetch("https://jsonplaceholder.typicode.com/users");
 // // const data = await response.json();
 // console.log(a);

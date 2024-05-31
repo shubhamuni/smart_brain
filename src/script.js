079d22139504739da7259364a67d48c358bde9c5
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-31 08:55:46 +0530

    check

--- src/script.js
+++ src/script.js
@@ -1,38 +1,40 @@
-const express = require("express")
-const fs = require("fs")
+const fs = require('fs')
 
-console.time("startcheck") //to check time required to process code from console.time() to console.timeEnd()
+// 
+// const fs = require("fs")
 
-fs.readFile('./hello.txt',(err, data) => {
-    if(err){
-        console.log('Error',err)
-    }
-    console.log('Async', data.toString('utf-8'))
-})
-const file = fs.readFileSync('./hello.txt');
-console.log('Sync',file.toString())
-//Below line adds " this is test" sentence to end of hello.txt content
-fs.appendFile('./hello.txt', " this is test", err => console.log(err))
-fs.writeFile("bye.txt","Sad to see you bye",err => 
-  {  if (err)  {
-    console.log(err)
-}})
-fs.unlink("./bye.txt",err => 
-    {  if (err)  {
-      console.log(err)
-  }
-  console.log("Inception")
-})
+// console.time("startcheck") //to check time required to process code from console.time() to console.timeEnd()
+
+// fs.readFile('./hello.txt',(err, data) => {
+//     if(err){
+//         console.log('Error',err)
+//     }
+//     console.log('Async', data.toString('utf-8'))
+// })
+// const file = fs.readFileSync('./hello.txt');
+// console.log('Sync',file.toString())
+// //Below line adds " this is test" sentence to end of hello.txt content
+// fs.appendFile('./hello.txt', " this is test", err => console.log(err))
+// fs.writeFile("bye.txt","Sad to see you bye",err => 
+//   {  if (err)  {
+//     console.log(err)
+// }})
+// fs.unlink("./bye.txt",err => 
+//     {  if (err)  {
+//       console.log(err)
+//   }
+//   console.log("Inception")
+// })
  
-const app = express();
+// const app = express();
 
-app.use(express.static(__dirname + '../../public'))
+// app.use(express.static(__dirname + '../../public'))
 
-app.use((req, res, next)=>{
-    console.log("Hello")
-}) 
-app.listen(3001);
-console.timeEnd("startcheck") //to check time required to process code from console.time() to console.timeEnd()
+// app.use((req, res, next)=>{
+//     console.log("Hello")
+// }) 
+// app.listen(3001);
+// console.timeEnd("startcheck") //to check time required to process code from console.time() to console.timeEnd()
 // let a;
 // if(true) {
 //     const { largeNumber } = await import("./script1.js");

37331d64705f9a27e556e1b2a559b7a60b37238c
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-06-04 20:14:08 +0530

    console.time for checking

--- src/script.js
+++ src/script.js
@@ -23,20 +23,22 @@ question1();
 //2 - When does santa first enter the basement 
 function question2 () {
     fs.readFile('./santa.txt', (err, data)=> {
+        console.time('santa-time');
         const directions = data.toString();
         const directionsArray = directions.split('');
         let accumulator = 0;
         let counter = 0;
-        const answer = directionsArray.some((currentValue) => {
-            if (currentValue === '(') {
-                return accumulator += 1;
+        const answer = directionsArray.some((currentItem) => {
+            if (currentItem === '(') {
+                accumulator += 1;
             }
-            else if (currentValue === ')') {
-                return accumulator -= 1;
+            else if (currentItem === ')') {
+                accumulator -= 1;
             }
             counter++
             return accumulator < 0;
         })
+        console.timeEnd('santa-time');
         console.log('Basement entered at: ',counter);
     })
 }

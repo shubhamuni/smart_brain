937a52ec1133ce465cda50bc58dd6e864d1a1f9c
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-08-10 17:19:30 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ const displayFaceBox = (box) => {
               displayFaceBox(calculateFaceLocation(result.outputs[0].data.regions[0].region_info.bounding_box));
             // console.log(result)
             })
-      .catch(error => console.log('error occured', error));
+      .catch(error => console.log('error occured that is ', error));
   };
 
   const onInputChange = (event) => {
